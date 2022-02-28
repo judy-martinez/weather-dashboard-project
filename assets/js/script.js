f1da5f90c3a933f6cf1775c9884659b864702f13
Judy <judy.mtz1@gmail.com>
2022-02-27 22:25:27 -0600

    added forecast cards & script

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,7 +54,7 @@ var formSubmitHandler = function(event) {
 
 
 
-            // ADD DATA TO THE WEBPAGE
+            // ADD DATA TO THE WEBPAGE leave this alone
             let cityDisplay = document.querySelector('.cityName')
             let date = document.querySelector('.date')
             let tempDisplay = document.querySelector('#temp')
@@ -68,6 +68,73 @@ var formSubmitHandler = function(event) {
             windDisplay.innerHTML = `Wind: ${wind} mph`
             humidityDisplay.innerHTML = `Humidity: ${humidity}%`
             uviDisplay.innerHTML = `UV Index: ${uvi}`
+// _____________________________________________________________________________
+            // ADD DATA TO THE WEBPAGE
+            let date1 = document.querySelector('#date1')
+            let tempDisplay1 = document.querySelector('#temp1')
+            let windDisplay1 = document.querySelector('#wind1')
+            let humidityDisplay1 = document.querySelector('#humidity1')
+ 
+
+                        // cityDisplay1.innerHTML = `${data.name}`
+            date1.innerHTML = `${moment().format('L')}`
+            tempDisplay1.innerHTML = `Temp: ${temp1}&#8457;`
+            windDisplay1.innerHTML = `Wind: ${wind1} mph`
+            humidityDisplay1.innerHTML = `Humidity: ${humidity1}%`
+
+
+            // ADD DATA TO THE WEBPAGE
+            let date2 = document.querySelector('#date2')
+            let tempDisplay2 = document.querySelector('#temp2')
+            let windDisplay2 = document.querySelector('#wind2')
+            let humidityDisplay2 = document.querySelector('#humidity2')
+
+
+                        // cityDisplay2.innerHTML = `${data.name}`
+            date2.innerHTML = `${moment().format('L')}`
+            tempDisplay2.innerHTML = `Temp: ${temp2}&#8457;`
+            windDisplay2.innerHTML = `Wind: ${wind2} mph`
+            humidityDisplay2.innerHTML = `Humidity: ${humidity2}%`
+
+
+            // ADD DATA TO THE WEBPAGE
+            let date3 = document.querySelector('#date3')
+            let tempDisplay3 = document.querySelector('#temp3')
+            let windDisplay3 = document.querySelector('#wind3')
+            let humidityDisplay3 = document.querySelector('#humidity3')
+
+                        // cityDisplay3.innerHTML = `${data.name}`
+            date3.innerHTML = `${moment().format('L')}`
+            tempDisplay3.innerHTML = `Temp: ${temp3}&#8457;`
+            windDisplay3.innerHTML = `Wind: ${wind3} mph`
+            humidityDisplay3.innerHTML = `Humidity: ${humidity3}%`
+
+            // ADD DATA TO THE WEBPAGE
+            let date4 = document.querySelector('#date4')
+            let tempDisplay4 = document.querySelector('#temp4')
+            let windDisplay4 = document.querySelector('#wind4')
+            let humidityDisplay4 = document.querySelector('#humidity4')
+
+                        // cityDisplay4.innerHTML = `${data.name}`
+            date4.innerHTML = `${moment().format('L')}`
+            tempDisplay4.innerHTML = `Temp: ${temp4}&#8457;`
+            windDisplay4.innerHTML = `Wind: ${wind4} mph`
+            humidityDisplay4.innerHTML = `Humidity: ${humidity4}%`
+
+            // ADD DATA TO THE WEBPAGE
+             // cityDisplay5 = document.querySelector('.cityName')
+            let date5 = document.querySelector('#date5')
+            let tempDisplay5 = document.querySelector('#temp5')
+            let windDisplay5 = document.querySelector('#wind5')
+            let humidityDisplay5 = document.querySelector('#humidity5')
+
+                        // cityDisplay5.innerHTML = `${data.name}`
+            date5.innerHTML = `${moment().format('L')}`
+            tempDisplay5.innerHTML = `Temp: ${temp5}&#8457;`
+            windDisplay5.innerHTML = `Wind: ${wind5} mph`
+            humidityDisplay5.innerHTML = `Humidity: ${humidity5}%`
+
+
         })
     })
 };
