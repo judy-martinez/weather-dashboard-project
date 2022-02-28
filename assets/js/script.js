5634377143888c35e48cb446adf027251fd0339f
Judy <judy.mtz1@gmail.com>
2022-02-27 23:13:02 -0600

    updated moment to display correct dates for 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,39 +22,32 @@ var formSubmitHandler = function(event) {
             let humidity = data2.daily[0].humidity
             let uvi = data2.daily[0].uvi
 
-            // FORECAST DAY 1, WEATHER data2
+            // FORECAST DAY 1, WEATHER data1
             let temp1 = data2.daily[1].temp.day
             let wind1 = data2.daily[1].wind_speed
             let humidity1 = data2.daily[1].humidity
-            let uvi1 = data2.daily[1].uvi
             
-            // FORECAST DAY 1, WEATHER data2
+            // FORECAST DAY 2, WEATHER data2
             let temp2 = data2.daily[2].temp.day
             let wind2 = data2.daily[2].wind_speed
             let humidity2 = data2.daily[2].humidity
-            let uvi2 = data2.daily[2].uvi
 
-            // FORECAST DAY 1, WEATHER data2
+            // FORECAST DAY 3, WEATHER data3
             let temp3 = data2.daily[3].temp.day
             let wind3 = data2.daily[3].wind_speed
             let humidity3 = data2.daily[3].humidity
-            let uvi3 = data2.daily[3].uvi
 
-            // FORECAST DAY 1, WEATHER data2
+            // FORECAST DAY 4, WEATHER data4
             let temp4 = data2.daily[4].temp.day
             let wind4 = data2.daily[4].wind_speed
             let humidity4 = data2.daily[4].humidity
-            let uvi4 = data2.daily[4].uvi
 
-            // FORECAST DAY 1, WEATHER DATA
+            // FORECAST DAY 5, WEATHER DATA 5
             let temp5 = data2.daily[5].temp.day
             let wind5 = data2.daily[5].wind_speed
             let humidity5 = data2.daily[5].humidity
-            let uvi5 = data2.daily[5].uvi
 
-
-
-            // ADD DATA TO THE WEBPAGE leave this alone
+            // ADD DATA TO THE WEBPAGE (CURRENT WEATHER)
             let cityDisplay = document.querySelector('.cityName')
             let date = document.querySelector('.date')
             let tempDisplay = document.querySelector('#temp')
@@ -68,20 +61,22 @@ var formSubmitHandler = function(event) {
             windDisplay.innerHTML = `Wind: ${wind} mph`
             humidityDisplay.innerHTML = `Humidity: ${humidity}%`
             uviDisplay.innerHTML = `UV Index: ${uvi}`
-// _____________________________________________________________________________
-            // ADD DATA TO THE WEBPAGE
+
+            // 5-DAY FORECAST
+
+            // ADD DATA TO THE WEBPAGE (TOMORROW)
             let date1 = document.querySelector('#date1')
             let tempDisplay1 = document.querySelector('#temp1')
             let windDisplay1 = document.querySelector('#wind1')
             let humidityDisplay1 = document.querySelector('#humidity1')
- 
 
-                        // cityDisplay1.innerHTML = `${data.name}`
-            date1.innerHTML = `${moment().format('L')}`
+            // cityDisplay1.innerHTML = `${data.name}`
+            date1.innerHTML = `${moment().add(1, 'days').format(`L`)}`
             tempDisplay1.innerHTML = `Temp: ${temp1}&#8457;`
             windDisplay1.innerHTML = `Wind: ${wind1} mph`
             humidityDisplay1.innerHTML = `Humidity: ${humidity1}%`
-
+            
+            // -------------------------------------- //
 
             // ADD DATA TO THE WEBPAGE
             let date2 = document.querySelector('#date2')
@@ -90,12 +85,13 @@ var formSubmitHandler = function(event) {
             let humidityDisplay2 = document.querySelector('#humidity2')
 
 
-                        // cityDisplay2.innerHTML = `${data.name}`
-            date2.innerHTML = `${moment().format('L')}`
+            // cityDisplay2.innerHTML = `${data.name}`
+            date2.innerHTML = `${moment().add(2, 'days').format(`L`)}`
             tempDisplay2.innerHTML = `Temp: ${temp2}&#8457;`
             windDisplay2.innerHTML = `Wind: ${wind2} mph`
             humidityDisplay2.innerHTML = `Humidity: ${humidity2}%`
 
+            // -------------------------------------- //
 
             // ADD DATA TO THE WEBPAGE
             let date3 = document.querySelector('#date3')
@@ -103,11 +99,13 @@ var formSubmitHandler = function(event) {
             let windDisplay3 = document.querySelector('#wind3')
             let humidityDisplay3 = document.querySelector('#humidity3')
 
-                        // cityDisplay3.innerHTML = `${data.name}`
-            date3.innerHTML = `${moment().format('L')}`
+            // cityDisplay3.innerHTML = `${data.name}`
+            date3.innerHTML = `${moment().add(3, 'days').format(`L`)}`
             tempDisplay3.innerHTML = `Temp: ${temp3}&#8457;`
             windDisplay3.innerHTML = `Wind: ${wind3} mph`
             humidityDisplay3.innerHTML = `Humidity: ${humidity3}%`
+
+            // -------------------------------------- //
 
             // ADD DATA TO THE WEBPAGE
             let date4 = document.querySelector('#date4')
@@ -115,21 +113,23 @@ var formSubmitHandler = function(event) {
             let windDisplay4 = document.querySelector('#wind4')
             let humidityDisplay4 = document.querySelector('#humidity4')
 
-                        // cityDisplay4.innerHTML = `${data.name}`
-            date4.innerHTML = `${moment().format('L')}`
+            // cityDisplay4.innerHTML = `${data.name}`
+            date4.innerHTML = `${moment().add(4, 'days').format(`L`)}`
             tempDisplay4.innerHTML = `Temp: ${temp4}&#8457;`
             windDisplay4.innerHTML = `Wind: ${wind4} mph`
             humidityDisplay4.innerHTML = `Humidity: ${humidity4}%`
 
+            // -------------------------------------- //
+
             // ADD DATA TO THE WEBPAGE
-             // cityDisplay5 = document.querySelector('.cityName')
+            // cityDisplay5 = document.querySelector('.cityName')
             let date5 = document.querySelector('#date5')
             let tempDisplay5 = document.querySelector('#temp5')
             let windDisplay5 = document.querySelector('#wind5')
             let humidityDisplay5 = document.querySelector('#humidity5')
 
-                        // cityDisplay5.innerHTML = `${data.name}`
-            date5.innerHTML = `${moment().format('L')}`
+            // cityDisplay5.innerHTML = `${data.name}`
+            date5.innerHTML = `${moment().add(5, 'days').format(`L`)}`
             tempDisplay5.innerHTML = `Temp: ${temp5}&#8457;`
             windDisplay5.innerHTML = `Wind: ${wind5} mph`
             humidityDisplay5.innerHTML = `Humidity: ${humidity5}%`
